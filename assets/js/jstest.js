b7454e356fb5092b12662acfb5facec43997215b
Jeremy Mundell <jeremiahmundell@gmail.com>
2017-08-14 16:04:55 -0400

    initial files

---
+++ assets/js/jstest.js
@@ -0,0 +1,7 @@
+var arr = []
+while(arr.length < 8){
+    var randomnumber = Math.ceil(Math.random()*100)
+    if(arr.indexOf(randomnumber) > -1) continue;
+    arr[arr.length] = randomnumber;
+}
+console.log(arr);
